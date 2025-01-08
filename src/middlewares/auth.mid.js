c12f2028529f522fd81ed15200ed356c2ec37ec1
Jinson25 <am987406@gmail.com>
2025-01-07 23:26:53 -0500

    refactor: reemplazar middleware de autenticación TypeScript por JavaScript

--- src/middlewares/auth.mid.js
+++ src/middlewares/auth.mid.js
@@ -1,12 +1,13 @@
 import { verify } from "jsonwebtoken";
 import { HTTP_UNAUTHORIZED } from "../constants/http_status";
 
-export default (req: any, res: any, next: any) => {
-    const token = req.headers.access_token as string;
+export default (req, res, next) => {
+    const token = req.headers.access_token;
     if (!token) return res.status(HTTP_UNAUTHORIZED).send();
 
     try {
-        const decodedUser = verify(token, process.env.JWT_SECRET!) as any;
+        const decodedUser = verify(token, process.env.JWT_SECRET);
+        
         req.user = decodedUser;
         //const user = { ...decodedUser, isAdmin: decodedUser.isAdmin };
         //req.user = user;
